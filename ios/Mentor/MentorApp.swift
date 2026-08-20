919db1a26b247585c5f485c5eb325ae7ec76ea68
Ahmad mahmoud <ahmad.shraim24@gmail.com>
2026-08-20 14:23:49 -0400

    Add iOS SwiftUI app skeleton

---
+++ ios/Mentor/MentorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentorApp.swift
+//  Mentor
+//
+//  Created by user293510 on 8/20/26.
+//
+
+import SwiftUI
+
+@main
+struct MentorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/MentorTests/MentorTests.swift
@@ -0,0 +1,18 @@
+//
+//  MentorTests.swift
+//  MentorTests
+//
+//  Created by user293510 on 8/20/26.
+//
+
+import Testing
+
+struct MentorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
